7fe93ced224833ccbaefd0b9f7f8706f37a93ada
TrkWebDesign <tarikk.abdijanovic@gmail.com>
2025-04-16 22:27:17 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -418,7 +418,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Function to send order confirmation email
     function sendOrderConfirmationEmail(orderDetails) {
-        const recipient = 'tarik.abdijanvic@gmail.com';
+        const recipient = 'tarikk.abdijanvic@gmail.com';
         const subject = 'Nova Narudžba';
         
         // Build the email body
